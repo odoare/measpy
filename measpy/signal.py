d06c5c63a44024d770107bb2a10ffd11522534c4
Olivier Doaré <nolv@free.fr>
2021-05-23 17:51:35 +0200

    dBA and dBC implemented as constants

--- measpy/signal.py
+++ measpy/signal.py
@@ -25,10 +25,6 @@ from measpy._tools import add_step
 # - Test Functions for all basic weightings (dBA, dBC)
 # - Improve plotting functions
 
-PREF = 20e-6*Unit('Pa') # Acoustic pressure reference level
-DBUREF = 1*Unit('V')
-DBVREF = np.sqrt(2)*Unit('V')
-
 ##################
 ##              ##
 ## Signal class ##
@@ -887,12 +883,12 @@ class Spectral:
         )  
     
     def apply_dBA(self):
-        w = Weighting.from_csv('measpy/data/dBA.csv')
-        return self.apply_weighting(w)
+        #w = Weighting.from_csv('measpy/data/dBA.csv')
+        return self.apply_weighting(WDBA)
 
     def apply_dBC(self):
-        w = Weighting.from_csv('measpy/data/dBC.csv')
-        return self.apply_weighting(w)
+        #w = Weighting.from_csv('measpy/data/dBC.csv')
+        return self.apply_weighting(WDBC)
 
     def dB_SPL(self):
         return self.unit_to(Unit(PREF)).similar(
@@ -1219,6 +1215,96 @@ class Weighting:
         return 20*np.log10(np.abs(self.a))
 
     # END of Weighting
+
+# Constants
+
+PREF = 20e-6*Unit('Pa') # Acoustic pressure reference level
+DBUREF = 1*Unit('V')
+DBVREF = np.sqrt(2)*Unit('V')
+
+WDBA = [
+    [6.3,-85.4],
+    [8,-77.8],
+    [10,-70.4],
+    [12.5,-63.4],
+    [16,-56.7],
+    [20,-50.5],
+    [25,-44.7],
+    [31.5,-39.4],
+    [40,-34.6],
+    [50,-30.2],
+    [63,-26.2],
+    [80,-22.5],
+    [100,-19.1],
+    [125,-16.1],
+    [160,-13.4],
+    [200,-10.9],
+    [250,-8.6],
+    [315,-6.6],
+    [400,-4.8],
+    [500,-3.2],
+    [630,-1.9],
+    [800,-0.8],
+    [1000,0],
+    [1250,0.6],
+    [1600,1.0],
+    [2000,1.2],
+    [2500,1.3],
+    [3150,1.2],
+    [4000,1.0],
+    [5000,0.5],
+    [6300,-0.1],
+    [8000,-1.1],
+    [10000,-2.5],
+    [12500,-4.3],
+    [16000,-6.6],
+    [20000,-9.3]]
+WDBA = Weighting(
+    f=np.array(WDBA)[:,0],
+    a=10**(np.array(WDBA)[:,1]/20),
+    desc='dBA weightings')
+
+WDBC = [
+    [6.3,-21.3 ],
+    [8,-17.7 ],
+    [10,-14.3 ],
+    [12.5 ,-11.2 ],
+    [16,-8.5 ],
+    [20,-6.2 ],
+    [25,-4.4 ],
+    [31.5 ,-3.0 ],
+    [40,-2.0 ],
+    [50,-1.3 ],
+    [63,-0.8 ],
+    [80,-0.5 ],
+    [100,-0.3 ],
+    [125,-0.2 ],
+    [160,-0.1 ],
+    [200,0.0 ],
+    [250,0.0 ],
+    [315,0.0 ],
+    [400,0.0 ],
+    [500,0.0 ],
+    [630,0.0 ],
+    [800,0.0 ],
+    [1000,0.0],
+    [1250,0.0 ],
+    [1600,-0.1 ],
+    [2000,-0.2 ],
+    [2500,-0.3 ],
+    [3150,-0.5 ],
+    [4000,-0.8 ],
+    [5000,-1.3 ],
+    [6300,-2.0 ],
+    [8000,-3.0 ],
+    [10000,-4.4 ],
+    [12500,-6.2 ],
+    [16000,-8.5 ],
+    [20000,-11.2 ]]
+WDBC = Weighting(
+    f=np.array(WDBC)[:,0],
+    a=10**(np.array(WDBC)[:,1]/20),
+    desc='dBC weightings')
 
 # Below are functions that may be useful (some cleaning should be done)
 
