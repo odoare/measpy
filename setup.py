0c9fa78a7a653bd36295568fe6bb58100ed1154e
Olivier Doaré <nolv@free.fr>
2021-06-21 22:13:59 +0200

    Update to version 0.0.6.1

--- setup.py
+++ setup.py
@@ -4,9 +4,23 @@ from datetime import datetime
 
 now = datetime.now()
 VERSION = 'git'+now.strftime("%Y%m%d")
-VERSION = '0.0.5'
+VERSION = '0.0.6.1'
 DESCRIPTION = 'Measurements with Python'
-LONG_DESCRIPTION = 'Classes and methods to do data acquisition and processing'
+LONG_DESCRIPTION = """
+    measpy is a set of classes and methods to help the data acquisition and analysis of signals. It is mainly acoustics and vibrations oriented. This package is very recent, it is still incomplete and many bugs may appear.
+
+    The base classes are:
+
+    - ```Signal```: It is basically a class that contain a 1D numpy array, an a few other properties to describe the data as: sampling frequency, calibration and unit.
+    - ```Spectral```: A spectral data class that contains the complex amplitudes as a 1D numpy array for frequencies up to the Nyquist frequency, and some properties as sampling frequency, unit, description
+    - ```Measurement``` : A class that describe a data acquisition process, its outputs (Signal objects), its inputs (Signal objects)...
+    - ```Weighting``` : Weighting spectral functions (Not yet fully test/functionnal)
+
+    For now, these daq devices are implemented :
+
+    - Audio cards, via the ```sounddevice``` package,
+    - NI DAQ cards, via the ```nidaqmx``` package.
+    """
 
 # Setting up
 setup(
