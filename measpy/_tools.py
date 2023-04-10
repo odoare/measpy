9f1dc2d95c4a2541a4e7e503b8b227c6d4de41c8
Olivier Doaré <olivier.doare@ensta-paris.fr>
2023-04-10 18:32:19 +0200

    New: wrap(phase) and unwrap_around_index(phase)

--- measpy/_tools.py
+++ measpy/_tools.py
@@ -5,6 +5,7 @@
 # (c) OD, 2021
 
 import csv
+import numpy as np
 
 def csv_to_dict(filename):
     """ Conversion from a CSV (produced by the class Measurement) to a dict
@@ -35,4 +36,13 @@ def convl1(fun,xx):
 
 def add_step(a,b):
     return a+'\n -->'+b
-    
+
+def wrap(phase):
+    """ Opposite of np.unwrap   
+    """
+    return np.mod((phase + np.pi), (2 * np.pi)) - np.pi
+
+def unwrap_around_index(phase,n):
+    """ Opposite of np.unwrap   
+    """
+    return np.hstack((np.unwrap(phase[n-1::-1][::-1]),np.unwrap(phase[n:])))
