37f2cea9287f9c0e500ae849c8de840fa9b72996
Olivier Doaré <olivier.doare@ensta-paris.fr>
2023-04-14 22:44:02 +0200

    Move some functions to _tools.py

--- measpy/signal.py
+++ measpy/signal.py
@@ -28,7 +28,11 @@ import numbers
 import unyt
 from unyt import Unit
 
-from measpy._tools import add_step
+from measpy._tools import (add_step,
+                           smooth,
+                           nth_octave_bands,
+                           create_time,
+                           apply_fades)
 
 ##################
 ##              ##
@@ -300,7 +304,7 @@ class Signal:
         :rtype: measpy.signal.Signal
         """
         return self.similar(
-            raw=_apply_fades(self.raw,fades),
+            raw=apply_fades(self.raw,fades),
             desc=add_step(self.desc,"fades")
         )
 
@@ -2165,34 +2169,6 @@ WDBC = Weighting(
 
 # Below are functions that may be useful (some cleaning should be done)
 
-def picv(long):
-    """ Create a 1D-array of length long with a unitary peak in the middle """
-    return np.hstack((np.zeros(long),1,np.zeros(long-1)))
-
-def _create_time1(fs,dur):
-    return np.linspace(0,dur-1/fs,int(round(dur*fs)))  # time axis
-
-def _create_time2(fs,length):
-    return np.linspace(0,(length-1)/fs,length)  # time axis
-
-def create_time(fs,dur=None,length=None):
-    if dur==None and length==None:
-        raise Exception('dur=duration in s or length=number of samples must be specified.')
-    if dur!=None and length!=None:
-        raise Exception("dur and length can't be both specified.")
-    if dur!=None:
-        return _create_time1(fs,dur)
-    else:
-        return _create_time2(fs,length)
-
-def _apply_fades(s,fades):
-    if fades[0]>0:
-        s[0:fades[0]] = s[0:fades[0]] * ((-np.cos(np.arange(fades[0])/fades[0]*np.pi)+1) / 2)
-    if fades[1]>0:
-        s[-fades[1]:] = s[-fades[1]:] *  ((np.cos(np.arange(fades[1])/fades[1]*np.pi)+1) / 2)
-    return s
-
-
 def _noise(fs, dur, out_amp, freqs):
     """ Create band-limited noise """
     leng = int(dur*fs)
@@ -2272,20 +2248,6 @@ def _sine(fs, dur, out_amp, freq):
     s = out_amp*np.sin(2*np.pi*create_time(fs=fs,dur=dur)*freq)
     return(s)
 
-def smooth(in_array,l=20):
-    ker = np.ones(l)/l
-    return np.convolve(in_array,ker,mode='same')
-
-def nth_octave_bands(n,fmin=5,fmax=20000):
-    """ 1/nth octave band frequency range calculation """
-    nmin = int(np.ceil(n*np.log2(fmin*10**-3)))
-    nmax = int(np.ceil(n*np.log2(fmax*10**-3)))
-    indices = range(nmin,nmax+1)
-    f_centre = 1000 * (2**(np.array(indices)/n))
-    f2 = 2**(1/n/2)
-    f_upper = f_centre * f2
-    f_lower = f_centre / f2
-    return f_centre, f_lower, f_upper
 
 # def noise(fs, dur, out_amp, freqs, fades):
 #     """ Create band-limited noise """
