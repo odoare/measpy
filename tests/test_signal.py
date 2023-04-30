9ed534a852d1949e2248941baced21f04ca925d9
Olivier Doaré <olivier.doare@ensta-paris.fr>
2023-04-30 21:27:46 +0200

    Fix bug to to Signal.max() now property

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -1,6 +1,6 @@
 import unittest
 import sys
-sys.path.insert(0, "measpy")
+sys.path.insert(0, "./measpy")
 import measpy as mp
 import numpy as np
 import unyt
@@ -30,21 +30,21 @@ class TestSignal(unittest.TestCase):
             self.assertEqual(s.length,s.fs*s.dur,"Calculation of Signal.length is wrong ?")
             
     def test_signalops(self):
-        self.assertLess((s1-s1).max(),err_tol, "Difference of two equal signals should be zero" )
-        self.assertLess(((s1/s1)-1).max(),err_tol, "Difference of two equal signals should be zero" )
-        self.assertLess( abs((abs(-(s1/s1))-1).max()), err_tol, "Absolute value of a -1 constant signal should be one"  )
-        self.assertLess( abs((abs(-(s1*(1/s1)))-1).max()), err_tol, "Absolute value of a -1 constant signal should be one"  )
+        self.assertLess((s1-s1).max,err_tol, "Difference of two equal signals should be zero" )
+        self.assertLess(((s1/s1)-1).max,err_tol, "Ratio of two equal signals should be one" )
+        self.assertLess( abs((abs(-(s1/s1))-1).max), err_tol, "Absolute value of a -1 constant signal should be one"  )
+        self.assertLess( abs((abs(-(s1*(1/s1)))-1).max), err_tol, "Absolute value of a -1 constant signal should be one"  )
 
     def test_corr(self):
-        self.assertLess(abs(abs(s1.corr(s1.similar(values=np.roll(s1.values,s1.fs))).tmax())-1*unyt.s), err_tol, "Max of correlation between signals should be at 1 sec.")
-        self.assertLess(abs(abs(s1.add_silence(extrat=(1,0)).corr(s1.add_silence(extrat=(0,1))).tmax())-1*unyt.s), err_tol, "Max of correlation between signals should be at 1 sec.")
+        self.assertLess(abs(abs(s1.corr(s1.similar(values=np.roll(s1.values,s1.fs))).tmax)-1*unyt.s), err_tol, "Max of correlation between signals should be at 1 sec.")
+        self.assertLess(abs(abs(s1.add_silence(extrat=(1,0)).corr(s1.add_silence(extrat=(0,1))).tmax)-1*unyt.s), err_tol, "Max of correlation between signals should be at 1 sec.")
         self.assertLess(abs(abs((s1.timelag(s1.similar(values=np.roll(s1.values,s1.fs)))))-1), err_tol, "Calculated timelag should be 1 sec.")
 
     def test_normalize(self):
-        self.assertEqual(s1.normalize().max(), 1.0, "Should be one")
+        self.assertEqual(s1.normalize().max, 1.0, "Should be one")
 
     def test_rms(self):
-        self.assertLess(s1.rms()-np.sqrt(2), err_tol, "RMS of 1V sine should be sqrt(2)")
+        self.assertLess(s1.rms-np.sqrt(2), err_tol, "RMS of 1V sine should be sqrt(2)")
 
     def test_hilbert(self):
         self.assertLess((s3.timelag(s3.hilbert())-1/(4*s3.fs)),err_tol, "Hilbert transform should induce a pi/2 (1/4 period) phase shift")
@@ -56,8 +56,8 @@ class TestSignal(unittest.TestCase):
             self.assertEqual(s.rfft().dur,dur,"Calculation of Spectral.dur is wrong ?")
             self.assertEqual(s.fft().freqs[1],1/s.dur,"Calculation of frequency vector is wrong ?")
             self.assertLess(min(abs(s.rfft(norm='ortho').values_at_freqs(range(30,19000,100))))-1,err_tol,"Noise spectrum has to be unitary in the (20,20000) frequency range")
-            self.assertLess((s.rfft().irfft()-s).rms(),err_tol,"RFFT + IRFFT gets the same signal")
-            self.assertLess((s.fft().ifft()-s).rms(),err_tol,"FFT + IFFT gets the same signal")
+            self.assertLess((s.rfft().irfft()-s).rms,err_tol,"RFFT + IRFFT gets the same signal")
+            self.assertLess((s.fft().ifft()-s).rms,err_tol,"FFT + IFFT gets the same signal")
             self.assertEqual(s.fft().sample_number,s.length,"After FFT the sample number of the original signal is correct")
             self.assertEqual(s.rfft().sample_number,s.length,"After RFFT the sample number of the original signal is correct")
             
@@ -73,7 +73,7 @@ class TestSignal(unittest.TestCase):
     def test_importexport(self):
         filname = 'test'+str(int(np.random.random()*10000))
         s1.to_csvwav(filname)
-        self.assertLess((s1-mp.Signal.from_csvwav(filname)).max(),err_tol, "Difference of two equal signals should be zero" )
+        self.assertLess((s1-mp.Signal.from_csvwav(filname)).max,err_tol, "Difference of two equal signals should be zero" )
         remove(filname+".csv")
         remove(filname+".wav")
 
