05692209d13c4eec41b65144aa77354071f2312c
Olivier Doaré <olivier.doare@ensta-paris.fr>
2023-05-01 20:52:34 +0200

    Adapt to new file organization

--- measpy/__init__.py
+++ measpy/__init__.py
@@ -1,11 +1,17 @@
 # __init__.py
 
-from measpy.signal import (Signal, 
-                            Spectral,
-                            Weighting,
-                            PREF,
-                            DBUREF,
-                            DBVREF,
-                            WDBA,
-                            WDBC)
+# import unyt
+# from unyt import Unit
+
+from measpy.signal import Signal 
+from measpy.spectral import Spectral 
+from measpy.weighting import Weighting
+from measpy.constants import (PREF,
+                                VREF,
+                                DBUREF,
+                                DBVREF,
+                                WDBA,
+                                WDBC)
 from measpy.measurement import Measurement
+
+#import measpy.constants
