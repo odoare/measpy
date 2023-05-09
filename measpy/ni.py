0678a3aac1cf9c3896a8ccebe27bfaafa806a636
Olivier Doaré <olivier.doare@ensta-paris.fr>
2023-05-09 22:56:28 +0200

    ni_run_task() implementation

--- measpy/ni.py
+++ measpy/ni.py
@@ -11,12 +11,13 @@
 import nidaqmx
 import nidaqmx.constants as niconst
 
-from ._tools import picv
+from ._tools import picv, siglist_to_array,t_min
 
 import numpy as np
 from numpy.matlib import repmat
 
 from datetime import datetime
+from time import sleep
 
 def _n_to_ain(n):
     return 'ai'+str(n-1)
@@ -29,6 +30,137 @@ def _callback(task_handle, every_n_samples_event_type,
 
     return 0
 
+def ni_run_task(M):
+    system = nidaqmx.system.System.local()
+
+    if M.device_type!='ni':
+        print("Warning: deviceType != 'ni'. Changing to 'ni'.")
+        M.device_type='ni'
+    if M.in_device=='':
+        print("Warning: no output device specified, changing to "+system.devices[0].name)
+        M.in_device=system.devices[0].name
+
+    if hasattr(M, 'in_range'):
+        if M.in_range == None:
+            inr = False
+        else :
+            inr = True
+    else:
+        inr = False
+    if not(inr):
+        val = nidaqmx.system.device.Device(M.in_device).ai_voltage_rngs[-1]
+        print("Warning: no input range specified, changing to the max value of "+M.in_device+" -> "+str(val))
+        M.in_range = list(val for b in M.in_map)
+
+    if M.out_sig!=None:
+        outx = siglist_to_array(M.out_sig)
+        tmin = t_min(M.out_sig)
+        if M.out_device=='':
+            print("Warning: no output device specified, changing to "+system.devices[0].name)
+            M.out_device=system.devices[0].name
+        if hasattr(M, 'out_range'):
+            if M.out_range == None:
+                outr = False
+            else :
+                outr = True
+        else:
+            outr = False
+        if not(outr):
+            val = nidaqmx.system.device.Device(M.out_device).ao_voltage_rngs[-1]
+            print("Warning: no output range specified, changing to the max value of "+M.out_device+" -> "+str(val))
+            M.out_range = list(val for b in M.out_map)
+    else:
+        tmin = 0
+
+    now = datetime.now()
+    M.date = now.strftime("%Y-%m-%d")
+    M.time = now.strftime("%H:%M:%S")
+
+    # Set up the read tasks
+    if M.in_sig!=None:
+        intask = nidaqmx.Task(new_task_name="in") # read task
+        for i,n in enumerate(M.in_map):
+            print(_n_to_ain(n))
+            intask.ai_channels.add_ai_voltage_chan(
+                physical_channel=M.in_device + "/" + _n_to_ain(n),
+                terminal_config=niconst.TerminalConfiguration.DEFAULT,
+                min_val=-M.in_range[i], max_val=M.in_range[i],
+                units=niconst.VoltageUnits.VOLTS)
+
+        intask.timing.cfg_samp_clk_timing(
+            rate=M.fs,
+            sample_mode=niconst.AcquisitionType.CONTINUOUS,
+            samps_per_chan=M.dur*M.fs)
+
+    # Set up the write tasks
+    if M.out_sig!=None:
+        outtask = nidaqmx.Task(new_task_name="out") # write task
+
+        # Set up the write tasks, use the sample clock of the Analog input if possible
+        for i,n in enumerate(M.out_map):   
+            outtask.ao_channels.add_ao_voltage_chan(
+                physical_channel=M.out_device + "/" + _n_to_aon(n), 
+                min_val=-M.out_range[i], max_val=M.out_range[i],
+                units=niconst.VoltageUnits.VOLTS)
+      
+        if M.in_device.startswith('myDAQ'):
+            # If the device is a myDAQ card, we keep most default values
+            # The myDAQ devices are set up separately because
+            # there is no error messages when setting up properties
+            # that are not supported, and the acquisition then fails
+            print("This is a NI my DAQ device")
+            outtask.timing.cfg_samp_clk_timing(
+                rate=M.fs,
+                sample_mode=niconst.AcquisitionType.CONTINUOUS,
+                samps_per_chan=nsamps)
+        else:
+            try:
+                # We first try to use analog input sample clock as output clock
+                outtask.timing.cfg_samp_clk_timing(
+                    rate=M.fs,
+                    source="/" + M.in_device + "/ai/SampleClock", #"OnboardClock",
+                    sample_mode=niconst.AcquisitionType.CONTINUOUS,
+                    samps_per_chan=nsamps)
+                print('Use of /'+ M.in_device + '/ai/SampleClock as output clock : success !')
+            except:
+                # If it fails, use defaults
+                # Then the in/out are not synchronized
+                # There is hence the possibility to use one analog input
+                # to do the in/out sync (io_sync=input channel number)
+                print("Error when choosing \""+"/" + M.in_device + "/ai/SampleClock\" as clock source, let's try \"OnboardClock\" ")
+                outtask.timing.cfg_samp_clk_timing(
+                    rate=M.fs,
+                    sample_mode=niconst.AcquisitionType.CONTINUOUS,
+                    samps_per_chan=nsamps)
+
+        if len(M.out_map)==1:
+            outtask.write(outx[:,0], auto_start=False)
+        else:
+            outtask.write(outx.T, auto_start=False)
+
+        outtask.start() # Start the write task first, waiting for the analog input sample clock
+
+    if M.in_sig!=None:
+        y = intask.read(M.fs*M.dur,timeout=M.dur+10) # Start the read task
+        intask.close()
+    else:
+        sleep(M.dur+10)
+
+    if M.out_sig!=None:
+        outtask.close()
+
+    if M.in_sig!=None:
+        # y=np.array(y).T
+        # if len(M.in_map)==1:
+        #     M.data[M.in_name[0]].raw = y
+        # else:
+        #     n=0
+        #     for s in M.in_name: 
+        #         M.data[s].raw = y[:,n]
+        #         n+=1
+        for i,s in enumerate(M.in_sig):
+            s.raw = np.array(y[:,i])
+            s.t0 = tmin
 
 def ni_run_measurement(M):
     """
