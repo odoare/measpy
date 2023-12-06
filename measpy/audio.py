0efc7cd16bd7a49ff71faedd5ad21831de1305ab
odoare <olivier.doare@ensta-paris.fr>
2023-12-06 16:47:49 +0100

    CopyPaste for Streaming method

--- measpy/audio.py
+++ measpy/audio.py
@@ -122,3 +122,78 @@ def audio_get_devices():
    """
    return sd.query_devices()
  
+def audio_run_measurement_stream(M):
+    """
+    Runs a measurement defined in the object of
+    the class measpy.measurement.Measurement given
+    as argument.
+
+    Once the data acquisition process is terminated,
+    the measurement object given in argument contains
+    a property in_sig consisting of a list of signals.
+
+    :param M: The measurement object that defines the measurement properties
+
+    :type M: measpy.measurement.Measurement
+
+    :return: Nothing, the measurement passed as argument is modified in place.
+
+    """
+
+    if M.device_type!='audio':
+        print("Warning: deviceType != 'audio'. Changing to 'audio'.")
+        M.device_type='audio'
+    if M.in_device=='':
+        print("Warning: no device specified, changing to None")
+        M.in_device=None
+    if M.out_sig!=None:
+        if M.out_device=='':
+            print("Warning: no device specified, changing to None")
+            M.out_device=None
+
+    now = datetime.now()
+    M.date = now.strftime("%Y-%m-%d")
+    M.time = now.strftime("%H:%M:%S")
+
+    # Set the audio devices to use
+    # And prepare the output arrays
+    if M.out_sig!=None:
+        outx = siglist_to_array(M.out_sig)
+        tmin = t_min(M.out_sig)
+        if M.in_sig!=None:
+            sd.default.device=(M.in_device,M.out_device)
+        else:
+            sd.default.device=(M.out_device)
+    else:
+        tmin = 0
+        if M.in_sig!=None:
+            sd.default.device=(M.in_device)
+        else:
+            raise Exception('No input nor output defined.')
+
+    if M.out_sig==None:
+        # in_stream = sd.InputStream()
+        y = sd.rec(int(M.dur * M.fs),
+                samplerate=M.fs,
+                mapping=M.in_map,
+                blocking=False)
+    else:
+        if M.in_sig==None:
+            y = sd.play(outx,
+                    samplerate=M.fs,
+                    mapping=M.out_map,
+                    blocking=False)
+        else:
+            y = sd.playrec(outx,
+                    samplerate=M.fs,
+                    input_mapping=M.in_map,
+                    output_mapping=M.out_map,
+                    blocking=False)
+            
+    sd.wait()
+
+    if M.in_sig!=None:
+        print(M.in_sig)
+        for i,s in enumerate(M.in_sig):
+            s.raw = np.array(y[:,i])
+            s.t0 = tmin
