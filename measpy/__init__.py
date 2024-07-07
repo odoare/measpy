3aca13a68bb4c5ef23095fa3cd22fba5836e365f
Doaré Olivier <olivier.doare@ensta-paris.fr>
2024-07-07 22:20:21 +0200

    Import utils

--- measpy/__init__.py
+++ measpy/__init__.py
@@ -11,6 +11,9 @@ from .signal import (Signal,
                             WDBM)
 from .measurement import Measurement
 
+from .utils import (mic_calibration_level,
+                        mic_calibration_freq)
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
