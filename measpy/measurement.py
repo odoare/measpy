0500dc3dcde4b397e2f5cbc1b569a65c503a978e
Clément SAVARO <clement.savaro@ensta-paris.fr>
2025-09-11 17:41:28 +0200

    better chuncksize

--- measpy/measurement.py
+++ measpy/measurement.py
@@ -317,11 +317,13 @@ class Measurement:
         self.filename = filename
 
     def create_hdf5(self, filename, chunck_size=0, datatype=None, dbfs=None):
+        maxchunsize = self.fs*self.dur
         filename = ensure_new_filename(filename)
         mesu = self._to_dict()
         in_sig = mesu.pop("in_sig",None)
         out_sig = mesu.pop("out_sig",None)
         data_type = mesu.pop("data_type",datatype)
+        chunck_size = min(maxchunsize,chunck_size) or maxchunsize
         with h5py.File(filename, "x") as H5file:
             for name, value in mesu.items():
                 if value is not None:
